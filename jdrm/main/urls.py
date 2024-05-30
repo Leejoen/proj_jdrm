7875b465a672fe94be2789240a785f32383c60e9
Alina Karpova <92012078+Leejoen@users.noreply.github.com>
2024-05-30 20:27:51 +0300

    new commit

--- jdrm/main/urls.py
+++ jdrm/main/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    # path(),
+    path('main', views.main),
+    # path('users', views.users),
+    # path('users', views.audits),
+    # path('users', views.events),
 ]
