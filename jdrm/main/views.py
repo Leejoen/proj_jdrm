3e09a8081a0d800b827e45ca3bb8c18ce7fcd08f
MalvadoPerro <diman_1205@mail.ru>
2024-06-08 17:55:21 +0300

    upd active navbar

--- jdrm/main/views.py
+++ jdrm/main/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.template.defaulttags import register
 from django.http import HttpResponse
+
+@register.filter
+def get_name_url(path):
+    return path.replace('/', '')
 
 def main(request):
     return render(request, 'main/main.html')
